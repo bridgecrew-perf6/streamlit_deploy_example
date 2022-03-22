5b42baf5a915ae21b8c89b3ee33cfdf0fc4d49c9
Thomas Yue <tclyue@gmail.com>
2022-03-22 10:26:07 -0700

    commit with code and plot

---
+++ app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+df = pd.read_csv('Data/NVDA.csv')
+print(df.head(10))
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+st.plotly_chart(fig)
+st.write('Enter welcome to streamlit deploy example')
+txt = st.text_input(label='Enter something here')
+st.write(txt)
