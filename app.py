8200721f4afd70df5e07dfbc72f67db5c5847027
Thomas Yue <tclyue@gmail.com>
2022-03-22 15:27:00 -0700

    streamlit tag

--- app.py
+++ app.py
@@ -19,5 +19,17 @@ st.plotly_chart(fig)
 st.write('Enter welcome to streamlit deploy example')
 txt = st.text_input(label='Enter something here')
 st.write(txt)
+
+# test streamlit tag
+from streamlit_tags import st_tags
+keywords = st_tags(
+    label='# Enter Keywords:',
+    text='Press enter to add more',
+    value=['Zero', 'One', 'Two'],
+    suggestions=['five', 'six', 'seven',
+                 'eight', 'nine', 'three',
+                 'eleven', 'ten', 'four'],
+    maxtags = 4,
+    key='1')
 #https://carpentries-incubator.github.io/python-interactive-data-visualizations/08-publish-your-app/index.html
 #pip3 freeze > requirements.txt
