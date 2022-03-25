6932f810005084691ddefe38ecce8859c8502e6d
Thomas Yue <tclyue@gmail.com>
2022-03-25 13:05:30 -0700

    without logo

--- dashboard_app.py
+++ dashboard_app.py
@@ -11,13 +11,13 @@ from multipage import MultiPage
 app = MultiPage()
 
 # Title of the main page
-display = Image.open('Logo.png')
-display = np.array(display)
+#display = Image.open('Logo.png')
+#display = np.array(display)
 # st.image(display, width = 400)
 # st.title("Data Storyteller Application")
-col1, col2 = st.columns(2)
-col1.image(display, width = 400)
-col2.title("Brite advisors board")
+#col1, col2 = st.columns(2)
+#col1.image(display, width = 400)
+#col2.title("Brite advisors board")
 
 # Add all your application here
 from dashboard_pages import test_multipage, test_multipage2
